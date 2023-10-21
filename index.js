c8734023b9a9635a14142859f9ca08d37630d3fc
Estevan <egonz94@gmail.com>
2023-10-21 18:48:22 -0500

    Added test files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello!');
+const inquirer = require('inquirer');
+
+criteria = {
+    message: 'Enter up to 3 characters',
+    type: 'input',
+    name: 'text'
+}
+
+inquirer.prompt(
+    criteria
+)
