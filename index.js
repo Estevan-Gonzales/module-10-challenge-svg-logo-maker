3610462856a1cdd93316a712a322db40f35d9a2e
Estevan <egonz94@gmail.com>
2023-10-23 20:10:53 -0500

    Implimented basic functionality

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-criteria = {
-    message: 'Enter up to 3 characters',
+const generateSVG = require('./lib/shapes.js');
+
+criteria = [
+    {message: 'Please enter up to 3 characters:',
     type: 'input',
-    name: 'text'
+    name: 'text'},
+
+    {message: 'Please enter a text color:',
+    type: 'input',
+    name: 'textColor'},
+
+    {message: 'Please choose a shape:',
+    type: 'list',
+    name: 'shape',
+    choices: ['circle', 'square', 'triangle']},
+
+    {message: 'Please enter a shape color:',
+    type: 'input',
+    name: 'shapeColor'}
+]
+
+
+
+function writeToFile(fileName, criteria) {
+    inquirer
+    .prompt(criteria).then((data) => {
+        fs.writeFile(fileName, generateSVG(data), (err) =>
+        err ? console.log(err) : console.log('Successfully created SVG file!')
+        )});
 }
 
-inquirer.prompt(
-    criteria
-)
+
+writeToFile('logo2.svg', criteria)
